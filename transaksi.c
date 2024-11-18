f2d9e439a10bf46379c86288b298c7ad489f23ef
naylakl <naylakl2406@gmail.com>
2024-11-18 16:25:09 +0700

    Create transaksi.c

---
+++ transaksi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "header.h"
+
+// Proses transaksi
+void processTransaction() {
+    printf("Fungsi Proses Transaksi\n");
+    // Logika untuk transaksi
+}
+
+// Laporan penjualan
+void generateSalesReport() {
+    printf("Fungsi Laporan Penjualan\n");
+    // Logika untuk laporan penjualan
+}
