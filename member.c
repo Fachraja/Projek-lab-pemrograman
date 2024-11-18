934960792a46f2911bba8c7852d0c3c7c0491732
naylakl <naylakl2406@gmail.com>
2024-11-18 16:24:28 +0700

    Update member.c

--- member.c
+++ member.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include "header.h"
 
+// Tambah member
 void addMember() {
-    char name[50];
-    int points;
+    printf("Fungsi Tambah Member\n");
+    // Logika untuk menambahkan member
+}
 
-    printf("Nama Member: ");
-    scanf("%s", name);
-    printf("Poin Awal: ");
-    scanf("%d", &points);
-
-    FILE *file = fopen("data/members.txt", "a");
-    if (!file) {
-        printf("File members.txt tidak ditemukan!\n");
-        return;
-    }
-    fprintf(file, "%s %d\n", name, points);
-    fclose(file);
-    printf("Member berhasil ditambahkan!\n");
+// Lihat daftar member
+void viewMembers() {
+    printf("Fungsi Lihat Member\n");
+    // Logika untuk melihat daftar member
 }
