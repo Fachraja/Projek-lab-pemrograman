f3e09ac1d6337e9a3733c15dfca5d5b8bb9732c9
naylakl <naylakl2406@gmail.com>
2024-11-18 16:22:54 +0700

    Update and rename header.c to header.h

---
+++ header.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// Fungsi untuk produk
+void addProduct();
+void deleteProduct();
+void viewProducts();
+void updateStock();
+void categorizeProducts();
+
+// Fungsi untuk member
+void addMember();
+void viewMembers();
+
+// Fungsi untuk transaksi
+void processTransaction();
+void generateSalesReport();
+
+// Fungsi untuk login
+void login();
+
+#endif
