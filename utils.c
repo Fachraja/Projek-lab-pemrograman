e619e253004b3851edc937d580e0b577c8d5381f
naylakl <naylakl2406@gmail.com>
2024-11-18 16:25:31 +0700

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "header.h"
+
+void displayMenu() {
+    printf("\n=== Sistem Kasir ===\n");
+    printf("1. Tambah Produk\n");
+    printf("2. Hapus Produk\n");
+    printf("3. Ubah Stok Produk\n");
+    printf("4. Lihat Daftar Produk\n");
+    printf("5. Kategorikan Produk\n");
+    printf("6. Proses Transaksi\n");
+    printf("7. Tambah Member\n");
+    printf("8. Laporan Penjualan\n");
+    printf("9. Keluar\n");
+}
